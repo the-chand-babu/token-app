c8298e8ed879d5f88f3b0607d4d2c4c3a8d4c07f
chand babu <jafri.nbj@gmail.com>
2023-06-24 16:06:53 +0530

    initially commit

---
+++ Front-End/src/App.jsx
@@ -0,0 +1,27 @@
+
+import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Login from './pages/login/Login';
+
+
+
+function App() {
+  
+
+  return (
+    <div className='App'>
+      <BrowserRouter>
+      
+      <Routes>
+        <Route path='/' element ={<Login />} />
+        
+
+      </Routes>
+      
+      </BrowserRouter>
+      
+    </div>
+  )
+}
+
+export default App
