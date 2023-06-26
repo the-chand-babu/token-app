953998681205e666cc2eb6e1c63d3c91dcb209dd
chand babu <jafri.nbj@gmail.com>
2023-06-26 15:17:13 +0530

    adding form and storing form data in backend

--- Backend/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-
-const connection = mongoose.connect("mongodb+srv://Admin:TechGlide@cluster0.jsk2z6k.mongodb.net/token?retryWrites=true&w=majority");
-
-module.exports ={connection}
-
-
